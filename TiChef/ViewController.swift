57a69693c932503103c51847c11a80c0d8a52d1c
DHorde <dimitri.horde@gmail.com>
2016-02-04 17:35:49 +0100

    Début de l'application
    Création de l’application, avec le background et les boutons de départ

---
+++ TiChef/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TiChef
+//
+//  Created by Dimitri Horde on 04/02/2016.
+//  Copyright © 2016 Dimitri Horde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
